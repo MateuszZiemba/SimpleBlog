9e2b75f565e62a91def7dcf594b374bcc5947699
Mateusz Ziemba <mateusz.ziemba@amister.pl>
2019-10-08 14:24:35 +0200

    Basic implementation of Disqus

--- SimpleBlog/Helpers/Extensions.cs
+++ SimpleBlog/Helpers/Extensions.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using SimpleBlog.Core.BusinessObjects;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace SimpleBlog //todo analyze
 {
@@ -13,5 +15,10 @@ namespace SimpleBlog //todo analyze
         //    var systemTimezone = TimeZoneInfo.FindSystemTimeZoneById(ConfigurationManager.AppSettings["Timezone"]);
         //    return String.Format("{0} ({1})", TimeZoneInfo.ConvertTimeFromUtc(utcDT, systemTimezone).ToShortDateString(), ConfigurationManager.AppSettings["TimezoneAbbreviation"]);
         //}
+
+        public static string Href(this Post post, UrlHelper helper)
+        {
+            return helper.RouteUrl(new { controller = "Blog", action = "Post", year = post.PublishedOn.Year, month = post.PublishedOn.Month, title = post.UrlSlug });
+        }
     }
 }
